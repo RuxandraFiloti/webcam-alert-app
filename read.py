2cc00554e02528cff37ca466e262331bcef941ea
Ruxandra <filotiruxandra@gmail.com>
2025-06-04 17:12:00 +0300

    removed some files

--- read.py
+++
@@ -1,7 +0,0 @@
-import cv2 #uses BGR not RGB 
-
-array = cv2.imread("image.png")
-
-print (array.shape) #displays the dimension of array -> e.g. (3,4,3) - 3 columns, 4 rows and 3 channels (BGR)
-
-print(array)
